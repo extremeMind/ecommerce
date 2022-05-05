bb734bf855c54d88a33edfcb5adcc3687df4a4d9
extremeMind <mjlemos99@gmail.com>
2022-05-05 16:16:53 +0100

    Account admin

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
 import express from "express";
 import cors from 'cors';
 import data from './data.js';
+import mongoose from 'mongoose';
+import config from "./config.js";
+import userRouter from "./routers/userRoute.js";
+
+
+mongoose.connect(config.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    }).then(() => {
+        console.log("Connected to mongodb.");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 const app = express();
 app.use(cors());
+app.use('/api/users', userRouter);
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
